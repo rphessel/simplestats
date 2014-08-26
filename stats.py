32df980bae2c6ea5ec85a849e15359fec6cab87e
Jared Knowles <jknowles@gmail.com>
2014-08-26 14:55:37 -0500

    I added a median

--- stats.py
+++ stats.py
@@ -1,13 +1,19 @@
 def mean(vals):
     """Computes the mean from a list of values."""
-    try:
-        total = float(sum(vals))
-        length = len(vals)
-    except TypeError:
-        raise TypeError("The list was not numbers.")
-    except:
-        print "Something unknown happened with the list."
+    total = float(sum(vals))
+    length = len(vals)
     return total/length
+
+def median(vals):
+    """Computes the median from a list of values."""
+    def median(vals):
+    vals.sort()
+    length = len(vals)
+    index = length / 2
+    if length % 2 == 0:
+       return mean([vals[index], vals[index - 1]])
+    else:
+       return vals[index]
 
 def mode(vals):
     """Computes the mode from a list of values."""
@@ -15,12 +21,11 @@ def mode(vals):
 
 def std(vals):
     """Computes the standard deviation from a list of values."""
+    # finally, some math
     n = len(vals)
     if n == 0:
         return 0.0
     mu = sum(vals) / n
-    if mu == 1e500:
-        return NotImplemented
     var = 0.0
     for val in vals:
         var = var + (val - mu)**2
